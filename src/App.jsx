c181c8e08ed85e561ba1a91c541a9b23da8d17b3
AurelienAKickersoft <aurelien.a@kickersoft.com>
2024-03-18 22:43:29 -0500

    Update build configurations and dependencies
    The Vite configurations have been revised with the new 'vite-plugin-lib-inject-css' plugin to increase CSS handling in the build process. The 'glob' utility has been added to process and map JS and JSX files. Furthermore, 'path' has been updated with more specific path utilities imported, and there were updates to the version and dependencies in the package.json file.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Hello from "./components/Hello/Hello.jsx";
+import Hello from "../lib/components/Hello/Hello.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
-import path from "path";
+import { extname, relative, resolve } from 'path'
+import { fileURLToPath } from 'node:url'
+import { glob } from 'glob'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import  {libInjectCss} from "vite-plugin-lib-inject-css";
@@ -7,14 +9,24 @@ import  {libInjectCss} from "vite-plugin-lib-inject-css";
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname,"src/index.js"),
+      entry: resolve(__dirname,"lib/index.js"),
       name: "first-components-library",
       fileName: (format) =>`index.${format}.js`,
     }
   },
   rollupOptions:{
     external:["react", "react-dom"],
+    input: Object.fromEntries(
+        glob.sync('lib/**/*.{js,jsx}').map(file => [
+            relative('lib',
+                file.slice(0, file.length - extname(file).length)
+            ),
+            fileURLToPath(new URL(file, import.meta.url))
+        ])
+    ),
     output: {
+      assetFileNames: 'asset/[name][extname]',
+      entryFileNames: '[name].js',
       globals: {
         react: "React",
         "react-dom": "ReactDOM"
