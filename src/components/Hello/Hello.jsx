5c9e0f156254f513f1f2222926733c56c1b51e96
AurelienAKickersoft <aurelien.a@kickersoft.com>
2024-03-18 22:19:15 -0500

    Add initial setup for Vite with React
    This commit includes the foundational elements to setup a new Vite project with React. It adds new files like .eslintrc.cjs, .gitignore, package.json, package-lock.json, index.html, and few others. Additionally, it consists of a number of components and stylesheets, set up for React development. The ESLint configuration, git ignore rules, and package dependencies have also been defined.

---
+++ src/components/Hello/Hello.jsx
@@ -0,0 +1,7 @@
+import styles from './Hello.module.scss'
+export default function Hello(){
+    return (<div className={styles.container}>
+        <p>Hello</p>
+    </div>)
+}
+
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname,"src/index.js"),
+      name: "first-components-library",
+      fileName: (format) =>`index.${format}.js`,
+    }
+  },
+  rollupOptions:{
+    external:["react", "react-dom"],
+    output: {
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM"
+      },
+    },
+  },
+  sourcemap: true,
+  emptyOutDir:true,
+  plugins: [react()],
+})
